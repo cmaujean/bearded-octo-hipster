9f589b42c4fea0c78e882cd19bc99255ff5be234
Erik Hollensbe <erik+github@hollensbe.org>
2012-04-18 20:14:51 -0700

    first stab at qr codes w/ ascii armor

--- app.rb
+++ app.rb
@@ -1,10 +1,22 @@
+require 'bundler/setup'
 require 'sinatra'
 require 'rqrcode'
 
-get '/' do
-  haml :index
+set :public_folder, File.dirname(__FILE__) + '/public'
+
+MAX_KEY_LENGTH = 4096
+QRCODE_SIZE = 40
+QRCODE_LEVEL = :m
+
+get '/ascii' do
+  haml :ascii_index
 end
 
-post '/' do
-  haml :qrcode
+post '/ascii' do
+  qrcode = params[:qrcode]
+  raise "too long" if qrcode.length > MAX_KEY_LENGTH
+
+  @qr = RQRCode::QRCode.new(qrcode, :size => QRCODE_SIZE, :level => QRCODE_LEVEL)
+
+  haml :ascii_qrcode
 end
